43ec6da1e28208679ff2b49dd41171f7c9a53d88
zhouMac <LazyZhou1997@gmail.com>
2018-04-05 14:52:02 +0800

    2018-4-5

--- src/main/java/lint/Lint14.java
+++ src/main/java/lint/Lint14.java
@@ -10,26 +10,41 @@ public class Lint14 {
 
     public static void main(String[] args) {
         Lint14 lint14 = new Lint14();
-        int[] nums = {1, 2, 3, 4, 5};
+        int[] nums = {2, 6, 8, 13, 15, 17, 17, 18, 19, 20};
 
-        System.out.println(lint14.binarySearch(nums,3));
+        System.out.println(lint14.binarySearch(nums, 15));
     }
 
     public int binarySearch(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
 
         int start = 0;
         int end = nums.length;
-        int mid;
+        int mid = 0;
+        boolean find = false;
 
-        while (start < end) {
+        while (start <= end) {
             mid = (start + end) / 2;
             if (nums[mid] > target) {
                 end = mid - 1;
             } else if (nums[mid] < target) {
                 start = mid + 1;
             } else {
-                return mid;
+
+                find = true;
+                break;
             }
+        }
+
+        //找到第一个元素
+        if (find) {
+            while (mid - 1 >= 0 && nums[mid - 1] == nums[mid]) {
+                mid--;
+            }
+
+            return mid;
         }
 
         return -1;
